1457b70a9108f47e1aafdbf75f44e565acf3f9d6
Parth Patel <104268600+parthu4u2@users.noreply.github.com>
2025-01-10 10:52:38 +0530

    Add files via upload

---
+++ parth/src/main/java/com/mycompany/parth/Parth.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+//  package com.mycompany.parth;
+ import java.sql.*;
+ import java.util.*;
+ public class Parth {
+ 
+     public static void main(String[] args) {
+         Scanner scan=new Scanner(System.in);
+
+         System.out.println("Enter User Name");
+         String un=scan.next();
+
+         System.out.println("Enter Password");
+         String pw=scan.next();
+
+         try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection con=DriverManager.getConnection("jdbc.mysql://localhost:3306/ajp","root","");
+
+            Statement stn=con.createStatement();
+             String query = "SELECT data FROM credit WHERE username='" + un + "' AND password='" + pw + "'";
+            ResultSet rs=stn.executeQuery(query);
+
+            if (rs.next()) {
+                System.out.println("Login Succsesfull Data"+rs.getString("data"));
+            } else {
+                System.out.println("Login Failed");
+            }
+
+            rs.close();
+            stn.close();
+            con.close();
+         } catch (ClassNotFoundException e) {
+            System.out.println("JDBC Driver Not Found,Add MYSQL.JDBC dependency");
+            e.printStackTrace();
+          }catch(SQLException e){
+            System.out.println("SQL Exception accurend."+e.getMessage());
+            e.printStackTrace();
+          }
+      }
+ }
+ 
