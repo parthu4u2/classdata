4ab8db0a37fca2629b444c8822e85556c602ed90
Parth Patel <104268600+parthu4u2@users.noreply.github.com>
2025-01-28 14:13:44 +0530

    Update 8 puzzle problem.py

--- 8 puzzle problem.py
+++ 8 puzzle problem.py
@@ -1,6 +1,6 @@
 def print_in_format(matrix):
-    for io in range(9):
-      if i%3==0 and i >0:
+    for i in range(9):
+      if i % 3==0 and i >0:
         print("")
       print(str(matrix[i])+" ",end="")
 
@@ -20,4 +20,62 @@ def move(ar,p,st):
   for i in range(len(ar)):
     dupl_st=st.copy()
     
+    tmp=dupl_st[p]
+    dupl_st[p]=dupl_st[ar[i]]
+    dupl_st[ar[i]]=tmp
     
+    trh=count(dupl_st)
+
+    if trh<rh:
+      rh=trh
+      store_st=dupl_st.copy()
+      
+  return store_st,rh
+
+state=[1,2,3,0,5,6,4,7,8]
+
+h=count(state)
+Level=1
+
+print("\n--------- Level "+str(Level)+"----------")
+print_in_format(state)
+print("\n Heuristic Value(Misplaced) :"+str(h))
+
+while h>0:
+  pos=int(state.index(0))
+  
+  Level +=1
+  
+  if pos==0:
+    arr=[1,3]
+    state,h=move(arr,pos,state)
+  elif pos==1:
+    arr=[0,2,4]
+    state,h=move(arr,pos,state)
+  elif pos==2:
+    arr=[1,5]
+    state,h=move(arr,pos,state)
+  elif pos==3:
+    arr=[0,4,6]
+    state,h=move(arr,pos,state)
+  elif pos==4:
+    arr=[1,3,5,7]
+    state,h=move(arr,pos,state)
+  elif pos==5:
+    arr=[2,4,8]
+    state,h=move(arr,pos,state)
+  elif pos==6:
+    arr=[3,7]
+    state,h=move(arr,pos,state)
+  elif pos==7:
+    arr=[4,6,8]
+    state,h=move(arr,pos,state)
+  elif pos==8:
+    arr=[5,7]
+    state,h=move(arr,pos,state)
+  
+  print("\n---------- Level "+str(Level)+"----------")
+  print_in_format(state)
+  print("\n Heuristic Value(Misplaced) :"+str(h))
+  
+  
