1b0ff11aef73bb8088dc0e62e5d72303ff40738d
Parth Patel <104268600+parthu4u2@users.noreply.github.com>
2025-01-30 09:15:00 +0530

    Add files via upload

---
+++ DCN_LAB_DATA/Practical8.c
@@ -0,0 +1,72 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void generateFrames(char *message, int frameLength)
+{
+    int msgLength = strlen(message);
+    int totalFrames = (msgLength + frameLength - 1) / frameLength;
+    int lastFrameSize = msgLength % frameLength;
+
+    if (lastFrameSize == 0)
+    {
+        lastFrameSize = frameLength;
+    }
+
+    printf("\n Total number of frames : %d\n", totalFrames);
+    printf("Frame length: %d\n\n", frameLength);
+
+    for (int i = 0; i < totalFrames; i++)
+    {
+        int currentFrameSize=(i==totalFrames-1)?lastFrameSize:frameLength;
+
+        printf("Frame %d: [", i + 1);
+        printf("Length: %d ]", currentFrameSize);
+
+        printf("Data :");
+
+        for (int j = 0; j < currentFrameSize && (i * frameLength + j) < msgLength; j++)
+        {
+        printf("%c",message[i*frameLength+j]);
+        }
+        printf("\n");
+    }
+}
+
+
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+void generateFrames(char *message, int frameLength)
+{
+    int msgLength = strlen(message);
+    int totalFrames = (msgLength + frameLength - 1) / frameLength;
+    int lastFrameSize = msgLength % frameLength;
+
+    if (lastFrameSize == 0)
+    {
+        lastFrameSize = frameLength;
+    }
+
+    printf("\n Total number of frames : %d\n", totalFrames);
+    printf("Frame length: %d\n\n", frameLength);
+
+    for (int i = 0; i < totalFrames; i++)
+    {
+        int currentFrameSize=(i==totalFrames-1)?lastFrameSize:frameLength;
+
+        printf("Frame %d: [", i + 1);
+        printf("Length: %d ]", currentFrameSize);
+
+        printf("Data :");
+
+        for (int j = 0; j < currentFrameSize && (i * frameLength + j) < msgLength; j++)
+        {
+        printf("%c",message[i*frameLength+j]);
+        }
+        printf("\n");
+    }
+}
